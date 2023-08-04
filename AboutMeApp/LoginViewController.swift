d394d22fb2f5c155cafdd2f9497fa976e202d6b7
CemenVy <semen.vydrin0108@gmail.com>
2023-08-04 18:00:38 +0400

    Work with logic for LoginViewController and WelcomeViewController

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var userNameTextField: UITextField?
+    @IBOutlet var passwordTextField: UITextField?
+    
+    private let myUserName = "Cemen"
+    private let myPassword = "12345"
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.greeting = "Welcom, \(userNameTextField?.text ?? "")!"
     }
 
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard let userName = userNameTextField?.text, !userName.isEmpty,
+              let userPassword = passwordTextField?.text, !userPassword.isEmpty else {
+            showAlert(withTitel: "Oops!", andMessage: "Please enter your login and password.")
+            return false
+        }
+        
+        if userNameTextField?.text == myUserName, passwordTextField?.text == myPassword {
+            return true
+        } else {
+            showAlert(withTitel: "Attention!", andMessage: "Your login or password is invalid.")
+            userNameTextField?.text = ""
+            passwordTextField?.text = ""
+            return false
+        }
+    }
+    
+    
+    @IBAction func forgotUserNameButtonTapped() {
+        showAlert(withTitel: "User name", andMessage: "Cemen")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showAlert(withTitel: "Password", andMessage: "12345")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField?.text = ""
+        passwordTextField?.text = ""
+    }
+
+    private func showAlert(withTitel titel: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: titel,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
